236ed18f52e9014f9e8372fa445d16ac5bc726e2
Scott <ScottishCyclops@users.noreply.github.com>
2017-02-28 22:37:01 +0100

    Added copyright

--- nvidia-overclock-default.sh
+++ nvidia-overclock-default.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+# -*- coding: UTF-8 -*-
+
+#   DistributedBlend is a software designed to render multiple blender
+#   files on multiple GPUs with the click of a button
+#   Copyright (C) 2017 Scott Winkelmann
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+# 
 
 #last 3 GPus get underclocked, the first one is simply reset
 #because it is beeing used for display
